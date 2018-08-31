33170c2be6ea4eb4e2ba0ca0d42a901f866a8c0e
Travis Briggs <audiodude@gmail.com>
2018-08-31 20:57:18 +0000

    Working barebones website

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
+$(function() {
+  function refreshFromHash() {
+    var hash = window.location.hash;
+    $('section').hide();
+    if (hash) {
+      $(hash).show();
+    } else {
+      $('#radio-machine').show();
+    }
+  }
 
+  $(window).on("hashchange", refreshFromHash);
+  refreshFromHash();
+});
